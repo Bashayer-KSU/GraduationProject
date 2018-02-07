aec7ea5913a438da587039239da839450e0b1740
BashayerKSU <twj.bashayer@gmail.com>
2018-02-07 17:48:25 +0300

    www

---
+++ Building Station/App_Code/RegisterLogin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for RegisterLogin
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+[System.Web.Script.Services.ScriptService]
+public class RegisterLogin : System.Web.Services.WebService
+{
+    string cs = ConfigurationManager.ConnectionStrings["DB"].ConnectionString;
+
+    public RegisterLogin()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+
+    [WebMethod]
+    public Boolean Register(String name, String email, String password)
+    {
+        using (SqlConnection con = new SqlConnection(cs))
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into shopowner (name, email, password) values " +
+                "('" + name + "','" + email + "','" + password + "')", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        //\insert selected colors to database
+
+        //JavaScriptSerializer js = new JavaScriptSerializer();
+        //Context.Response.Write(js.Serialize(selectedColors));
+        return true;
+    }
+
+}
---
+++ Building Station/RegisterLogin.js
@@ -0,0 +1,26 @@
+﻿var myApp = angular.module("myApp", []);
+
+var Register = app.controller("Register", function ($scope, $http) {
+    $http.get('RegisterLogin.asmx/Register')
+        .then(function (response) {
+            $scope.colors = response.data;
+        });
+});
+
+myApp.controller("RegisterCtrl", function ($scope, $location) {
+    $scope.loginCheck = function () {
+        var uname = $scope.user.name;
+        var pwd = $scope.user.password;
+        if (uname == 'admin' && pwd == 'admin123') {
+            alert("match");
+            // window.location.hash = '#/dashboard';
+            $location.path('/dashboard');
+
+        } else {
+            alert("not match");
+            //  $location.path('/about');
+            //	 location.reload();
+
+        }
+    };
+});
