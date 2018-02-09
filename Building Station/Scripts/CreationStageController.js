5905cb6be17a68b6811926932208913abf0e03de
Asmaa-KSU <36111823+Asmaa-KSU@users.noreply.github.com>
2018-02-09 17:14:13 +0300

    creation stage

---
+++ Building Station/Scripts/CreationStageController.js
@@ -0,0 +1,14 @@
+﻿
+
+var app = angular.module("Demo", ["ngRoute"])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when("/1", {
+            templateUrl: "CreationStagePages/1.html",
+            controller: ""
+            })
+            .when("/2", {
+                templateUrl: "CreationStagePages/2.html",
+                controller: ""
+            })
+    });
