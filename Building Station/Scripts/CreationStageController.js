4ef62b027a1bd9c7eacb10612ba47226e6eb7528
Asmaa-KSU <36111823+Asmaa-KSU@users.noreply.github.com>
2018-02-10 01:25:05 +0300

    +

--- Building Station/Scripts/CreationStageController.js
+++ Building Station/Scripts/CreationStageController.js
@@ -1,14 +1,33 @@
 ﻿
 
 var app = angular.module("Demo", ["ngRoute"])
-    .config(function ($routeProvider) {
+    .config(function ($routeProvider, $locationProvider) {
         $routeProvider
+            .when("/0", {
+                templateUrl: "CreationStagePages/0.html",
+                controller: "0Controller"
+            })
             .when("/1", {
-            templateUrl: "CreationStagePages/1.html",
-            controller: ""
+                templateUrl: "CreationStagePages/1.html",
+                controller: "1Controller"
             })
             .when("/2", {
                 templateUrl: "CreationStagePages/2.html",
-                controller: ""
+                controller: "2Controller"
             })
+            .otherwise({
+                redirectTo: "/0"
+            })
+        $locationProvider.html5Mode(true);
+    })
+    .controller("0Controller", function ($scope) {
+    })
+    .controller("1Controller", function ($scope) {
+        $scope.header = "What's your store name ?";
+        $scope.previous = "CreationStage.html";
+    })
+    .controller("2Controller", function ($scope) {
+        $scope.header = "What kind of online store do you want ?",
+            $scope.previous = "/1";
+
     });
