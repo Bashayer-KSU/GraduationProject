d90cbb5f51a737a272bcf57bbfe379ae3acc7407
Asmaa-KSU <36111823+Asmaa-KSU@users.noreply.github.com>
2018-02-10 23:59:33 +0300

    creation stage english

--- Building Station/Scripts/CreationStageController.js
+++ Building Station/Scripts/CreationStageController.js
@@ -15,6 +15,46 @@ var app = angular.module("Demo", ["ngRoute"])
                 templateUrl: "CreationStagePages/2.html",
                 controller: "2Controller"
             })
+            .when("/3.1", {
+                templateUrl: "CreationStagePages/3.1.html",
+                controller: "3.1Controller"
+            })
+            .when("/3.2", {
+                templateUrl: "CreationStagePages/3.2.html",
+                controller: "3.2Controller"
+            })
+            .when("/4.1", {
+                templateUrl: "CreationStagePages/4.1.html",
+                controller: "4.1Controller"
+            })
+            .when("/4.2", {
+                templateUrl: "CreationStagePages/4.2.html",
+                controller: "4.2Controller"
+            })
+            .when("/5", {
+                templateUrl: "CreationStagePages/5.html",
+                controller: "5Controller"
+            })
+            .when("/6", {
+                templateUrl: "CreationStagePages/6.html",
+                controller: "6Controller"
+            })
+            .when("/7", {
+                templateUrl: "CreationStagePages/7.html",
+                controller: "7Controller"
+            })
+            .when("/8", {
+                templateUrl: "CreationStagePages/8.html",
+                controller: "8Controller"
+            })
+            .when("/9", {
+                templateUrl: "CreationStagePages/9.html",
+                controller: "9Controller"
+            })
+            .when("/10", {
+                templateUrl: "CreationStagePages/10.html",
+                controller: "10Controller"
+            })
             .otherwise({
                 redirectTo: "/0"
             })
@@ -25,4 +65,24 @@ var app = angular.module("Demo", ["ngRoute"])
     .controller("1Controller", function ($scope) {
     })
     .controller("2Controller", function ($scope) {
+    })
+    .controller("3.1Controller", function ($scope) {
+    })
+    .controller("3.2Controller", function ($scope) {
+    })
+    .controller("4.1Controller", function ($scope) {
+    })
+    .controller("4.2Controller", function ($scope) {
+    })
+    .controller("5Controller", function ($scope) {
+    })
+    .controller("6Controller", function ($scope) {
+    })
+    .controller("7Controller", function ($scope) {
+    })
+    .controller("8Controller", function ($scope) {
+    })
+    .controller("9Controller", function ($scope) {
+    })
+    .controller("10Controller", function ($scope) {
     });
