12978afc7ef164892ec2290c982609f403b18964
Bashayer-KSU <435202741@student.ksu.edu.sa>
2018-02-24 05:23:01 +0300

    template_6

--- Building Station/RegisterLogin.js
+++ Building Station/RegisterLogin.js
@@ -1,46 +1,6 @@
 ﻿var myApp = angular.module("myApp", []);
 
-var Register = myApp.controller("Register", function ($scope, $http) {
-    $http.get('RegisterLogin.asmx/Register')
-        .then(function (response) {
-            $scope.colors = response.data;
-        });
-});
-/*
-myApp.controller("RegisterCtrl", function ($scope, $location, $http) {
-    $scope.RegisterCheck = function () {
-        var uname = $scope.user.name;
-        var pwd = $scope.user.password;
-        var email = $scope.email;
-        var phone = $scope.phone;
-        alert("I'm in the script");
-
-        if (uname === 'admin' && pwd === 'admin123') {
-            alert("match");
-            // window.location.hash = '#/dashboard';
-            $location.path('/dashboard');
-
-        } else {
-            alert("not match");
-            //  $location.path('/about');
-            //	 location.reload();
-
-        }
-        $http({
-            url: "RegisterLogin.asmx/Register",
-            method: "get",
-            params: { name: uname, email: email, password: pwd, phone: phone }
-        })
-            .then(function (response) {
-                $scope.result = response.data;
-            }, function (error) {
-                $scope.error = error.data;
-            });
-    };
-});
-*/
-
-myApp.controller("RegisterLoginCtrl", function ($scope, $http) {
+var Register = myApp.controller("RegisterLoginCtrl", function ($scope, $http) {
 
     $scope.SendData = function (e) {
         // use $.param jQuery function to serialize data from JSON 
@@ -72,7 +32,8 @@ myApp.controller("RegisterLoginCtrl", function ($scope, $http) {
 
         $http.post(url, data, config)
             .then(function (response) {
-                $scope.result = response.data;
+                if (response.data === true)
+                    window.location.href = "/CreationStage.html";
             }, function (error) {
                 $scope.error = error.data;
             });
