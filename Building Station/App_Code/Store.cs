c11ed267e675fa51dc102235633517270f14ec1c
Asmaa-KSU <36111823+Asmaa-KSU@users.noreply.github.com>
2018-02-24 12:36:39 +0300

    trying to upload image

--- Building Station/App_Code/Store.cs
+++ Building Station/App_Code/Store.cs
@@ -8,26 +8,29 @@ using System.Web;
 /// </summary>
 public class Store
 {
-    public int ID { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string ShopOwner { get; set; }
     public string Name { get; set; }
     public string Type { get; set; }
     public string Logo { get; set; }
-    public string StoreText { get; set; }
+    public string Description { get; set; }
     public string SliderImage{ get; set; }
     public string Address { get; set; }
-    public string Email { get; set; }
     public string Phone { get; set; }
     public bool Published { get; set; }
     public string Domain { get; set; }
-    public string ShopOwner { get; set; }
     public string Color1 { get; set; }
     public string Color2 { get; set; }
     public string Color3 { get; set; }
     public string Color4 { get; set; }
     public List<string> SocialMedialinks { get; set; }
-    public string Password { get; set; }
-
-
+    public int TemplateID { get; set; }
+    public bool PayPal { get; set; }
+    public bool Cach { get; set; }
+    public bool BankTransfer { get; set; }
+    public string BankAccount { get; set; }
+    public string menuTitle { get; set; }
 
     public Store()
     {
