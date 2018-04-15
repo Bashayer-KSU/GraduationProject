8a151c4af3b39f967a4091c99f8b57494222bb23
Asmaa-KSU <36111823+Asmaa-KSU@users.noreply.github.com>
2018-04-15 10:37:30 +0300

    unit test

---
+++ BuildingStationLClassLibrary/ShowHideElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BuildingStationLClassLibrary
+{
+    public class ShowHideElement
+    {
+        public static Boolean ValidLink(string name)
+        {
+            string link = "";
+            if (name.ToLower() == "snapchat")
+                link = "https://www.snapchat.com/add/";
+            else if (name.ToLower() == "facebook")
+                link = "https://www.facebook.com/";
+            else if (name.ToLower() == "instagram")
+                link = "https://www.instagram.com/";
+            else if (name.ToLower() == "twitter")
+                link = "https://www.twitter.com/";
+
+            if (link != "")
+                return false;
+            else
+                return false;
+        }
+    }
+}
